3f0592f59af2b5c7c1ead59f19cb32c645636d3d
Muhammad-adam778 <Muhammad.adam4078@gmail.com>
2022-12-05 20:25:09 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Stats-preview-card-component
+# Frontend Mentor - Stats preview card component solution
+
+This is a solution to the [Stats preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### Screenshot
+
+![Desktop View](./screenshots/img-1.png)
+![Mobile View](./screenshots/img-2.png)
+
+### Demo
+
+https://muhammad-adam778.github.io/Stats-preview-card-component/
---
+++ main.js
@@ -0,0 +1,38 @@
+// Catch elements
+const numbersSpan = document.querySelectorAll(".stats p:first-child .num");
+const statsCard = document.querySelector("article.card");
+
+// Variables
+let numbersArray = Array.from(numbersSpan);
+let counterStarted = false;
+
+// ##Events
+// Show Card
+statsCard.classList.add("show");
+
+// On Desktop Devices
+if (window.navigator.maxTouchPoints === 0) {
+  numbersArray.forEach((num) => counter(num));
+}
+// On Touch Devices
+if (window.navigator.maxTouchPoints > 0) {
+  window.onscroll = function () {
+    if (scrollY > 50) {
+      numbersArray.forEach((num) => counter(num));
+    }
+  };
+}
+
+// ##Functions
+function counter(num) {
+  let goal = +num.dataset.number;
+  if (!counterStarted) {
+    let counter = setInterval(function () {
+      counterStarted = true;
+      num.innerHTML++;
+      if (+num.innerHTML === goal) {
+        clearInterval(counter);
+      }
+    }, 1800 / goal);
+  }
+}
